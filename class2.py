4d4daee904c573cbf04b7c22a78aaaaebd9fb9d5
Curtis Vinson <curtisvinson@curtiss-air.lan>
2023-02-26 20:11:00 -0500

    Initial commit

---
+++ class2.py
@@ -0,0 +1,4 @@
+username  = "your"
+print("hello there and welcome to the game," + username)
+str_two = "face"
+
---
+++ main.py
@@ -0,0 +1,21 @@
+# times = input("Clean up your room")
+# times = int(times)
+#
+# for num in range(0,101):
+#     if num % 2 == 0:
+#         print(num)
+for num in range(1,21):
+    if num % 2 == 0:
+        print(f"{num} is even")
+else:
+    print(f"{num} is odd")
+
+if num == 4 or num == 13:
+    print(f"{num} is unlucky")
+
+
+
+
+
+
+
